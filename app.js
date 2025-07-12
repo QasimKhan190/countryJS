e5dd3c1c338d2fa15952d01af6db122f2a28bd64
QasimKhan190 <qasimkamran17@gmail.com>
2025-07-12 21:21:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function abc(){
+    arr=["United States", "Canada", "Brazil", "Mexico", "United Kingdom","France", "Germany", "Italy", "Spain", "Russia","China", "India", "Japan", "South Korea", "Indonesia","Australia", "New Zealand", "South Africa", "Egypt", "Nigeria","Saudi Arabia", "Turkey", "Iran", "Pakistan", "Bangladesh","Argentina", "Colombia", "Ukraine", "Thailand", "Vietnam"]
+a=Math.round(Math.random()*arr.length)
+b=arr[a]
+c=document.getElementById('h')
+c.innerText=b
+
+
+
+
+
+s=b.split('');
+
+
+
+
+
+}
